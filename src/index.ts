44410baaee90e16a466123e9cc470e8fd4d4f55c
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-08-31 12:21:41 -0400

    :art: lint with eslint + prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-export function generateEventIdClientSide() {
-  if (typeof window !== "undefined") {
-    const gtmContainerId = "GTM-57QS65R";
-    if (typeof window?.google_tag_manager !== "undefined") {
+export function generateEventIdClientSide(): string | undefined {
+  if (typeof window !== 'undefined') {
+    const gtmContainerId = 'GTM-57QS65R';
+    if (typeof window?.google_tag_manager !== 'undefined') {
       var gtmData =
-        window.google_tag_manager[gtmContainerId].dataLayer.get("gtm");
-      return gtmData.start + "." + gtmData.uniqueEventId;
+        window.google_tag_manager[gtmContainerId].dataLayer.get('gtm');
+      return gtmData.start + '.' + gtmData.uniqueEventId;
     } else {
-      return window.crypto.randomUUID().replaceAll("-", "");
+      return window.crypto.randomUUID().replaceAll('-', '');
     }
   }
 }
@@ -17,94 +17,94 @@ export const sum = (a: number, b: number) => {
   return a + b;
 };
 export const ALLOWED_PROMO_EVENTS = [
-  "PageView",
-  "PromoEventAddPaymentInfo",
-  "PromoEventAddToCart",
-  "PromoEventCompleteRegistration",
-  "PromoEventDonate",
-  "PromoEventInitiateCheckout",
-  "PromoEventLead",
-  "PromoEventPurchase",
-  "PromoEventSearch",
-  "PromoEventStartTrial",
-  "PromoEventSubmitApplication",
-  "PromoEventSubscribe",
-  "PromoEventViewContent",
+  'PageView',
+  'PromoEventAddPaymentInfo',
+  'PromoEventAddToCart',
+  'PromoEventCompleteRegistration',
+  'PromoEventDonate',
+  'PromoEventInitiateCheckout',
+  'PromoEventLead',
+  'PromoEventPurchase',
+  'PromoEventSearch',
+  'PromoEventStartTrial',
+  'PromoEventSubmitApplication',
+  'PromoEventSubscribe',
+  'PromoEventViewContent',
 ];
 
 export const gtmPageView = (remainingData: object) => {
-  gtmEvent("PromoEventPageView", remainingData);
+  gtmEvent('PromoEventPageView', remainingData);
 };
 
 export const pageView = (remainingData: object) => {
-  gtmEvent("PageView", remainingData);
+  gtmEvent('PageView', remainingData);
 };
 
 export const promoEventPromoButtonClick = (remainingData: object) => {
-  gtmEvent("PromoEventPromoButtonClick", remainingData);
+  gtmEvent('PromoEventPromoButtonClick', remainingData);
 };
 
 export const promoEventButtonClick = (remainingData: object) => {
-  gtmEvent("PromoEventLoginButtonClick", remainingData);
+  gtmEvent('PromoEventLoginButtonClick', remainingData);
 };
 
 export const promoEventSignupButtonClick = (remainingData: object) => {
-  gtmEvent("PromoEventSignupButtonClick", remainingData);
+  gtmEvent('PromoEventSignupButtonClick', remainingData);
 };
 
 export const promoEventAddPaymentInfo = (remainingData: object) => {
-  gtmEvent("PromoEventAddPaymentInfo", remainingData);
+  gtmEvent('PromoEventAddPaymentInfo', remainingData);
 };
 
 export const promoEventDonate = (remainingData: object) => {
-  gtmEvent("PromoEventDonate", remainingData);
+  gtmEvent('PromoEventDonate', remainingData);
 };
 
 export const promoEventAddToCart = (remainingData: object) => {
-  gtmEvent("PromoEventAddToCart", remainingData);
+  gtmEvent('PromoEventAddToCart', remainingData);
 };
 
 export const promoEventCompleteRegistration = (remainingData: object) => {
-  gtmEvent("PromoEventCompleteRegistration", remainingData);
+  gtmEvent('PromoEventCompleteRegistration', remainingData);
 };
 
 export const promoEventInitiateCheckout = (remainingData: object) => {
-  gtmEvent("PromoEventInitiateCheckout", remainingData);
+  gtmEvent('PromoEventInitiateCheckout', remainingData);
 };
 
 export const promoEventLead = (remainingData: object) => {
-  gtmEvent("PromoEventLead", remainingData);
+  gtmEvent('PromoEventLead', remainingData);
 };
 
 export const promoEventPurchase = (remainingData: object) => {
-  gtmEvent("PromoEventPurchase", remainingData);
+  gtmEvent('PromoEventPurchase', remainingData);
 };
 
 export const promoEventSearch = (remainingData: object) => {
-  gtmEvent("PromoEventSearch", remainingData);
+  gtmEvent('PromoEventSearch', remainingData);
 };
 
 export const promoEventStartTrial = (remainingData: object) => {
-  gtmEvent("PromoEventStartTrial", remainingData);
+  gtmEvent('PromoEventStartTrial', remainingData);
 };
 
 export const promoEventSubmitApplication = (remainingData: object) => {
-  gtmEvent("PromoEventSubmitApplication", remainingData);
+  gtmEvent('PromoEventSubmitApplication', remainingData);
 };
 
 export const promoEventSubscribe = (remainingData: object) => {
-  gtmEvent("PromoEventSubscribe", remainingData);
+  gtmEvent('PromoEventSubscribe', remainingData);
 };
 
 export const promoEventViewContent = (remainingData: object) => {
-  gtmEvent("PromoEventViewContent", remainingData);
+  gtmEvent('PromoEventViewContent', remainingData);
 };
 
 const gtmEvent = (eventName: string, eventData: object) => {
   const eventId = generateEventIdClientSide();
   window.dataLayer?.push({
     event: eventName,
-    "x-fb-event_id": eventId,
+    'x-fb-event_id': eventId,
     transactionId: eventId,
     ...eventData,
   });
