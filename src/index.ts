aaeb74eef8c92535dae66c344a83c673b5d30df6
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-08-31 11:33:35 -0400

    :sparkles: add generateEventIdClientSide func w/typings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+export function generateEventIdClientSide() {
+  if (typeof window !== "undefined") {
+    const gtmContainerId = "GTM-57QS65R";
+    if (typeof window?.google_tag_manager !== "undefined") {
+      var gtmData =
+        window.google_tag_manager[gtmContainerId].dataLayer.get("gtm");
+      return gtmData.start + "." + gtmData.uniqueEventId;
+    } else {
+      return window.crypto.randomUUID().replaceAll("-", "");
+    }
+  }
+}
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('dev only output');
@@ -20,76 +32,76 @@ export const ALLOWED_PROMO_EVENTS = [
   "PromoEventViewContent",
 ];
 
-export const gtmPageView = (remainingData) => {
+export const gtmPageView = (remainingData: object) => {
   gtmEvent("PromoEventPageView", remainingData);
 };
 
-export const pageView = (remainingData) => {
+export const pageView = (remainingData: object) => {
   gtmEvent("PageView", remainingData);
 };
 
-export const promoEventPromoButtonClick = (remainingData) => {
+export const promoEventPromoButtonClick = (remainingData: object) => {
   gtmEvent("PromoEventPromoButtonClick", remainingData);
 };
 
-export const promoEventButtonClick = (remainingData) => {
+export const promoEventButtonClick = (remainingData: object) => {
   gtmEvent("PromoEventLoginButtonClick", remainingData);
 };
 
-export const promoEventSignupButtonClick = (remainingData) => {
+export const promoEventSignupButtonClick = (remainingData: object) => {
   gtmEvent("PromoEventSignupButtonClick", remainingData);
 };
 
-export const promoEventAddPaymentInfo = (remainingData) => {
+export const promoEventAddPaymentInfo = (remainingData: object) => {
   gtmEvent("PromoEventAddPaymentInfo", remainingData);
 };
 
-export const promoEventDonate = (remainingData) => {
+export const promoEventDonate = (remainingData: object) => {
   gtmEvent("PromoEventDonate", remainingData);
 };
 
-export const promoEventAddToCart = (remainingData) => {
+export const promoEventAddToCart = (remainingData: object) => {
   gtmEvent("PromoEventAddToCart", remainingData);
 };
 
-export const promoEventCompleteRegistration = (remainingData) => {
+export const promoEventCompleteRegistration = (remainingData: object) => {
   gtmEvent("PromoEventCompleteRegistration", remainingData);
 };
 
-export const promoEventInitiateCheckout = (remainingData) => {
+export const promoEventInitiateCheckout = (remainingData: object) => {
   gtmEvent("PromoEventInitiateCheckout", remainingData);
 };
 
-export const promoEventLead = (remainingData) => {
+export const promoEventLead = (remainingData: object) => {
   gtmEvent("PromoEventLead", remainingData);
 };
 
-export const promoEventPurchase = (remainingData) => {
+export const promoEventPurchase = (remainingData: object) => {
   gtmEvent("PromoEventPurchase", remainingData);
 };
 
-export const promoEventSearch = (remainingData) => {
+export const promoEventSearch = (remainingData: object) => {
   gtmEvent("PromoEventSearch", remainingData);
 };
 
-export const promoEventStartTrial = (remainingData) => {
+export const promoEventStartTrial = (remainingData: object) => {
   gtmEvent("PromoEventStartTrial", remainingData);
 };
 
-export const promoEventSubmitApplication = (remainingData) => {
+export const promoEventSubmitApplication = (remainingData: object) => {
   gtmEvent("PromoEventSubmitApplication", remainingData);
 };
 
-export const promoEventSubscribe = (remainingData) => {
+export const promoEventSubscribe = (remainingData: object) => {
   gtmEvent("PromoEventSubscribe", remainingData);
 };
 
-export const promoEventViewContent = (remainingData) => {
+export const promoEventViewContent = (remainingData: object) => {
   gtmEvent("PromoEventViewContent", remainingData);
 };
 
-const gtmEvent = (eventName, eventData) => {
-  const eventId = generateEventId();
+const gtmEvent = (eventName: string, eventData: object) => {
+  const eventId = generateEventIdClientSide();
   window.dataLayer?.push({
     event: eventName,
     "x-fb-event_id": eventId,
@@ -97,5 +109,3 @@ const gtmEvent = (eventName, eventData) => {
     ...eventData,
   });
 };
-
-export function 
