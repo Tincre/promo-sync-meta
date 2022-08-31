ca9ce9ec7d90e82c9619d988eaf4a6ce7fd14b91
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-08-31 12:22:22 -0400

    :white_check_mark: add generateEventIdClientSide coverage

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,35 @@
-import { sum } from '../src/index';
+import { generateEventIdClientSide, sum } from '../src/index';
+beforeAll(() => {
+  /* @ts-ignore */
+  global.window = jest.fn();
 
+  window['google_tag_manager'] = {
+    'GTM-57QS65R': {
+      dataLayer: {
+        get: (gtm: string) => {
+          return {
+            start: 'testStart',
+            uniqueEventId: 'testUniqueEventId',
+          };
+        },
+      },
+    },
+  };
+});
+
+describe('generateEventIdClientSide', () => {
+  it('returns a string', () => {
+    expect(generateEventIdClientSide()).toBe('testStart.testUniqueEventId');
+  });
+});
+describe('generateEventIdClientSide', () => {
+  it('returns undefined when google_tag_manager is not present on window', () => {
+    /* @ts-ignore */
+    global.window = undefined;
+
+    expect(generateEventIdClientSide()).toBe(undefined);
+  });
+});
 describe('sum', () => {
   it('adds two numbers together', () => {
     expect(sum(1, 1)).toEqual(2);
